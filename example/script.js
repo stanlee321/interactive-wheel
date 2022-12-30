47893a7c71015f6329a2cdb5f5da4c50ac937685
Stanley Salvatierra <s.salvatierra.bo@gmail.com>
2022-12-30 11:24:58 -0400

    updated text

--- example/script.js
+++ example/script.js
@@ -12,34 +12,51 @@ function positionImages() {
     const x = radius * Math.cos(radians);
     const y = radius * Math.sin(radians);
     image.style.transform = `translate(${x}px, ${y}px)`;
-    console.log(x,y)
+    // console.log(x,y)
   });  
 }
 
 positionImages()
 
-images.forEach(image => {
-  image.addEventListener('mouseenter', () => {
-    image.classList.add('rotate');
-  });
-  image.addEventListener('mouseleave', () => {
-    image.classList.remove('rotate');
-  });
-});
+// images.forEach(image => {
+//   image.addEventListener('mouseenter', () => {
+//     image.classList.add('rotate');
+//   });
+//   image.addEventListener('mouseleave', () => {
+//     image.classList.remove('rotate');
+//   });
+// });
 
 
 
 images.forEach(function(image) {
   // Change the appearance of the icon when the mouse hovers over it
   image.addEventListener('mouseover', function() {
-    icon.style.opacity = '0.5';
+    image.style.opacity = '0.5';
   });
   image.addEventListener('mouseout', function() {
     image.style.opacity = '1';
   });
 
   // Display more information about the SDG when the icon is clicked
-  image.addEventListener('click', function() {
-    alert('More information about ' + image.alt);
-  });
+  // image.addEventListener('click', function() {
+  //   alert('More information about ' + image.alt);
+  // });
+
+  // console.log('You clicked on image ' + image.alt);
+  // Refresh the page
 });
+
+
+for (let i = 0; i < images.length; i++) {
+  images[i].addEventListener('click', function() {
+    // Display the text on the right side of the screen
+    document.querySelector('#text').style.position = 'absolute';
+    document.querySelector('#text').style.right = '20px';
+    document.querySelector('#text').style.top = '20px';
+    document.querySelector('#text').textContent = 'You clicked on image ' + images[i].alt;;
+
+    // location.reload();
+
+  });
+}
